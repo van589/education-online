7e125b97d52b6a88a7b54c2db3feed9a4053a837
van589 <1273647531@qq.com>
2020-02-23 16:45:22 +0800

    新增删除单项信息功能

--- mooc-web-admin/src/main/resources/static/assets/app/adminApp.js
+++ mooc-web-admin/src/main/resources/static/assets/app/adminApp.js
@@ -111,6 +111,27 @@ var App = function () {
     };
 
     /**
+     * 删除单笔记录
+     * @param url 删除链接
+     * @param id 需要删除数据的 ID
+     */
+    var handlerDeleteSingle = function (url, id, msg) {
+        // 可选参数
+        if (!msg) msg = null;
+
+        // 将 ID 放入数组中，以便和批量删除通用
+        _idArray = new Array();
+        _idArray.push(id);
+
+        $("#modal-message").html(msg == null ? "您确定删除数据项吗？" : msg);
+        $("#modal-default").modal("show");
+        // 绑定删除事件
+        $("#btnModalOk").bind("click", function () {
+            handlerDeleteData(url);
+        });
+    };
+
+    /**
      * 批量删除
      * @param url
      */
@@ -139,50 +160,50 @@ var App = function () {
         $("#btnModalOk").bind("click", function () {
             handlerDeleteData(url);
         });
+    };
 
-        /**
-         * AJAX 异步删除
-         * @param url
-         */
-        var handlerDeleteData = function (url) {
-            $("#modal-default").modal("hide");
+    /**
+     * AJAX 异步删除
+     * @param url
+     */
+    var handlerDeleteData = function (url) {
+        $("#modal-default").modal("hide");
 
-            if (_idArray.length > 0) {
-                // AJAX 异步删除操作
-                setTimeout(function () {
-                    $.ajax({
-                        "url": url,
-                        "type": "POST",
-                        "data": {"ids": _idArray.toString()},
-                        "dataType": "JSON",
-                        "success": function (data) {
-                            // 请求成功后，无论是成功或是失败都需要弹出模态框进行提示，所以这里需要先解绑原来的 click 事件
-                            $("#btnModalOk").unbind("click");
+        if (_idArray.length > 0) {
+            // AJAX 异步删除操作
+            setTimeout(function () {
+                $.ajax({
+                    "url": url,
+                    "type": "POST",
+                    "data": {"ids": _idArray.toString()},
+                    "dataType": "JSON",
+                    "success": function (data) {
+                        // 请求成功后，无论是成功或是失败都需要弹出模态框进行提示，所以这里需要先解绑原来的 click 事件
+                        $("#btnModalOk").unbind("click");
 
-                            // 请求成功
-                            if (data.status === 200) {
-                                // 刷新页面
-                                $("#btnModalOk").bind("click", function () {
-                                    window.location.reload();
-                                });
-                            }
-
-                            // 请求失败
-                            else {
-                                // 确定按钮的事件改为隐藏模态框
-                                $("#btnModalOk").bind("click", function () {
-                                    $("#modal-default").modal("hide");
-                                });
-                            }
-
-                            // 因为无论如何都需要提示信息，所以这里的模态框是必须调用的
-                            $("#modal-message").html(data.message);
-                            $("#modal-default").modal("show");
+                        // 请求成功
+                        if (data.status === 200) {
+                            // 刷新页面
+                            $("#btnModalOk").bind("click", function () {
+                                window.location.reload();
+                            });
                         }
-                    });
-                }, 500)
-            }
-        };
+
+                        // 请求失败
+                        else {
+                            // 确定按钮的事件改为隐藏模态框
+                            $("#btnModalOk").bind("click", function () {
+                                $("#modal-default").modal("hide");
+                            });
+                        }
+
+                        // 因为无论如何都需要提示信息，所以这里的模态框是必须调用的
+                        $("#modal-message").html(data.message);
+                        $("#modal-default").modal("show");
+                    }
+                });
+            }, 500)
+        }
     };
 
     /**
@@ -219,6 +240,14 @@ var App = function () {
          */
         initDataTables: function (url, columns) {
             return handlerInitDataTable(url, columns);
+        },
+
+        /**
+         * 删除单笔数据
+         * @param url
+         */
+        deleteSingle: function(url, id, msg) {
+            handlerDeleteSingle(url, id, msg);
         },
 
         /**
